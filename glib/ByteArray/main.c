f08c7a20418e95354c916a7713fd8e29f0b09193
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2025-03-03 20:22:53 +0900

    ByteArray

---
+++ glib/ByteArray/main.c
@@ -0,0 +1,22 @@
+// refer https://gist.github.com/tetkuz/f45e9fe26e9da84509db70631b14e2da
+
+#include <string.h>
+#include <glib.h>
+
+int main()
+{
+	gchar *string = "message";
+	GByteArray *array;
+	guint8 *p;
+
+	array = g_byte_array_new ();
+	array = g_byte_array_append (array, (guint8 *)string, strlen(string));
+
+	p = g_byte_array_free (array, FALSE);
+
+	g_print ("%s\n", p);
+
+	g_free (p);
+
+	return 0;
+}
