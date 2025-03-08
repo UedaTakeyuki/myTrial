7e7999c15cb8d1e8fc7ca6537783a42de83d9349
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2025-03-08 18:48:48 +0900

    updated

--- c/simplest.c
+++ c/simplest.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[]) {
     CURL *curl = curl_easy_init();
     curl_easy_setopt(curl, CURLOPT_URL, "https://uedasoft.com");
-    CURLcode res = curl_easy_perform(gc_curl->curl);
+    CURLcode res = curl_easy_perform(curl);
     res = curl_easy_perform(curl);
     curl_easy_cleanup(curl);
 }
