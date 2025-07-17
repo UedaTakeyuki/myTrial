637ff39fe43f95ccf8a826fdec6d224b579135c9
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2025-07-17 21:44:49 +0900

    add myReact

---
+++ myReact/first-react-project/src/Alert.jsx
@@ -0,0 +1,18 @@
+export function Alert({ type = 'information', heading, children }) {
+  return (
+    <div>
+      <div>
+        <span role="img" aria-label={
+            type === 'warning'
+              ? 'Warning'
+              : 'Information'
+          }
+        >
+          {type === 'warning' ? '⚠' : 'ℹ️'}
+        </span>
+        <span>{heading}</span>
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+}
---
+++ myReact/first-react-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Alert } from './Alert'; 
+
+function App() {
+  return (
+    <Alert type="information" heading="Success">
+      Everything is really good!
+    </Alert>
+  );
+}
+
+export default App
