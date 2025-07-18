8f845da5bf6e5f1fbbf284bcba42b59e5ad732c8
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2025-07-18 12:00:42 +0900

    use status

--- myReact/first-react-project/src/Alert.jsx
+++ myReact/first-react-project/src/Alert.jsx
@@ -1,4 +1,10 @@
-export function Alert({ type = 'information', heading, children }) {
+import { useState } from 'react';
+
+export function Alert({ type = 'information', heading, children, closable }) {
+  const [visible, setVisible] = useState(true);
+  if (!visible) {
+    return null;
+  }
   return (
     <div>
       <div>
@@ -12,6 +18,13 @@ export function Alert({ type = 'information', heading, children }) {
         </span>
         <span>{heading}</span>
       </div>
+      {closable && (
+        <button aria-label="Close">
+          <span role="img" aria-label="Close">
+            ❌
+          </span>
+        </button>
+      )}
       <div>{children}</div>
     </div>
   );
--- myReact/first-react-project/src/App.jsx
+++ myReact/first-react-project/src/App.jsx
@@ -6,7 +6,7 @@ import { Alert } from './Alert';
 
 function App() {
   return (
-    <Alert type="information" heading="Success">
+    <Alert type="information" heading="Success" closable>
       Everything is really good!
     </Alert>
   );
