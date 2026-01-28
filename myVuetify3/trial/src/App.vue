4e25e96f57910611768f209d39293e2ccf4554f7
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2026-01-28 20:34:21 +0900

    add RouterLink

--- myVuetify3/trial/src/App.vue
+++ myVuetify3/trial/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <v-app>
     <v-main>
+      <RouterLink to="/">Go to Home</RouterLink>
+      <RouterLink to="/about">/Go to About</RouterLink>
       <router-view />
     </v-main>
   </v-app>
