39fc54206d46894d07d59f38ea21a87f1934d1a1
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2026-01-29 16:26:49 +0900

    add Application Bar

--- myVuetify3/trial/src/App.vue
+++ myVuetify3/trial/src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <v-app>
     <v-main>
+      <v-app-bar :elevation="2">
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template>
+
+        <v-app-bar-title>Application Bar</v-app-bar-title>
+      </v-app-bar>
       <RouterLink to="/">Go to Home</RouterLink>
       <RouterLink to="/about">/Go to About</RouterLink>
       <router-view />
