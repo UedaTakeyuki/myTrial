191aa802d7c2387e03c03ae3a81725f6c36dcd07
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2026-01-31 17:58:34 +0900

    add vuetify-nav3

--- myVuetify3/trial-pinia/src/layouts/default.vue
+++ myVuetify3/trial-pinia/src/layouts/default.vue
@@ -1,4 +1,7 @@
 <template>
+  <Navbar
+    titleStr="Demo App" :links="links" qrExp="けろけろせぶん"
+  />
   <v-main>
     <router-view />
   </v-main>
@@ -7,5 +10,10 @@
 </template>
 
 <script setup>
-  //
+import {Navbar} from 'vuetify-nav3'
+const links = [
+  { icon: 'home', text: 'Home', route: '/'},
+  { icon: 'about', text: 'About', route: '/about'},
+]
+//
 </script>
