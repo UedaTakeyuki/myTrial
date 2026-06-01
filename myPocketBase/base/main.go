b149fde648b12fe145a8054456ca4f1f1d0ec227
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2026-06-01 20:51:04 +0900

    add myPocketBase

---
+++ myPocketBase/base/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
+		// register new "GET /hello" route
+		se.Router.GET("/hello", func(se *core.RequestEvent) error {
+			return se.String(200, "ハゲがいるぞ殺せ!")
+		})
+		// serves static files from the provided public dir (if exists)
+		se.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), false))
+
+		return se.Next()
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
