0594c3234cfb9a419472c398b13230ff72b8166f
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2026-06-02 21:42:44 +0900

    add email & pw

--- myVuetify4/ui_for_pcketbase2/src/pages/about.vue
+++ myVuetify4/ui_for_pcketbase2/src/pages/about.vue
@@ -1,5 +1,28 @@
 <template>
-  <dev>
-  彡⌒ミがいるぞ殺せ！
-  </dev>
+  
+  <v-responsive
+    class="mx-auto"
+    max-width="344"
+  >
+    <p>email:{{email}}</p>
+    <p>pw:{{pw}}</p>
+    <v-text-field
+      v-model="email"
+      label="Email address"
+      placeholder="johndoe@gmail.com"
+      type="email"
+    />
+    <v-text-field
+      v-model="pw"
+      hint="Enter your password to access this website"
+      label="Password"
+      type="input"
+    ></v-text-field>
+  </v-responsive>
 </template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue'
+  const email = ref("kerokero@keroro.co.jp")
+  const pw = ref("1234")
+</script>
