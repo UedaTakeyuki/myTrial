7a0b39a8a32c69e79785bd86cb3264d6ace7f800
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2026-06-04 16:18:21 +0900

    show result of the server side verification

--- myVuetify4/ui_for_pcketbase2/src/components/SignUp.vue
+++ myVuetify4/ui_for_pcketbase2/src/components/SignUp.vue
@@ -18,11 +18,13 @@
         placeholder="johndoe@gmail.com"
         type="email"
       />
+      <span v-if="serversideErrors.email">{{ serversideErrors.email.message }}</span>
 
       <PwInput 
         v-model="pw"
         :rules=passwordRules
       />
+      <span v-if="serversideErrors.password">{{ serversideErrors.password.message }}</span>
 
       <PwInput 
         v-model="pw2"
@@ -30,12 +32,17 @@
         hint="Confirm password"
         :rules="[matchRule]"
       />
+      <span v-if="serversideErrors.passwordConfirm">{{ serversideErrors.passwordConfirm.message }}</span>
+      
+      <!-- 一般的なエラー（システムエラーなど）の表示 -->
+      <p v-if="serversideErrors.message" style="color: red;">{{ serversideErrors.message }}</p>
+
     </v-card-item>
     <v-card-actions class="justify-end">
       <v-btn
         icon="mdi-check"
         color="primary"
-        @click.once="login"
+        @click.once="signUpUser"
       >
       </v-btn>
     </v-card-actions>
@@ -47,25 +54,48 @@
   import PocketBase from 'pocketbase'
   import PwInput from '@/components/PwInput.vue'
 
+  // two-way bindings
   const email = ref()
   const pw = ref()
   const pw2 = ref()
   const name = ref()
-
-  const showPassword = ref(false)
+  const serversideErrors = ref({})
 
   // Sign Up
-  const login = async ()=>{
+  const signUpUser = async ()=>{
+    // registered
+    serversideErrors.value = {}
+
     const pb = new PocketBase('https://pocketbase.uedasoft.com');
-    const authData = await pb.collection("users").authWithPassword(email.value, pw.value);
-    location.reload();
+    const data = {
+      "email": email.value,
+      "password": pw.value,
+      "passwordConfirm": pw2.value,
+      "name": name.value
+    };
+
+    try {
+      // 'users' is the default auth collection in PocketBase
+      const record = await pb.collection('users').create(data);
+      
+      console.log('Successfully registered:', record.id);
+    } catch (err) {
+      console.error('Registration failed:', err.data);
+      if (err.response && err.response.data) {
+        serversideErrors.value = err.response.data;
+      } else {
+        serversideErrors.value = { message: '予期せぬエラーが発生しました。' };
+      }
+    }
   }
 
-  // Rules: value of pw2 should match with pw
+  // Rule: general pw rules
+  const passwordRules = [
+    v => !!v || 'Cannot be blank.',
+    v => (v && v.length >= 8) || 'Must be at least 8 character(s).',
+  ]
+
+  // Rule: value of pw2 should match with pw
   const matchRule = (value) => (value == pw.value) || 'password not match.'
 
-  const passwordRules = [
-    v => !!v || 'パスワードを入力してください。',
-    v => (v && v.length >= 8) || 'パスワードは8文字以上で入力してください。',
-  ]
 </script>
